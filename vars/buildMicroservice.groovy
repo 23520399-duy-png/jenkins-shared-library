1a0b6c9323f3dccd6313913bcbf0e23821c669d4
23520399-duy-png <23520399@gm.uit.edu.vn>
2026-05-25 05:05:13 +0000

    fix: remove global repository override to prevent ACR regression

--- vars/buildMicroservice.groovy
+++ vars/buildMicroservice.groovy
@@ -115,7 +115,6 @@ def call(Map config = [:]) {
                             wget -qO ./yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
                             chmod +x ./yq
 
-                            ./yq -i '.images.repository = "'${ACR_URL}'"' helm/online-boutique/values.yaml
                             ./yq -i '.'${SERVICE_NAME}'.image.tag = "'${IMAGE_TAG}'"' helm/online-boutique/values.yaml
 
                             git config user.name "Jenkins CI Bot"
