542a318a7ec9cb0f714b7cf077705041419dc407
23520399-duy-png <23520399@gm.uit.edu.vn>
2026-05-26 14:11:14 +0000

    fix yq variable expansion

--- vars/buildMicroservice.groovy
+++ vars/buildMicroservice.groovy
@@ -115,8 +115,9 @@ def call(Map config = [:]) {
                             wget -qO ./yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
                             chmod +x ./yq
 
-                            ./yq -i '.${SERVICE_NAME}.image.tag = "'${IMAGE_TAG}'"' helm/online-boutique/values.yaml
-                            ./yq -i '.${SERVICE_NAME}.image.repository = "'${ACR_URL}/${SERVICE_NAME}'"' helm/online-boutique/values.yaml
+                            ./yq -i ".${SERVICE_NAME}.image.tag = \\"${IMAGE_TAG}\\"" helm/online-boutique/values.yaml
+
+                            ./yq -i ".${SERVICE_NAME}.image.repository = \\"${ACR_URL}/${SERVICE_NAME}\\"" helm/online-boutique/values.yaml
 
                             git config user.name "Jenkins CI Bot"
                             git config user.email "jenkins@ci.local"
